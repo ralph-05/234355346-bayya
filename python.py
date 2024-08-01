a3ac117a6720a08255cf0b8dc97c5df4409696c6
ralph-05 <ralphlaurencebayya3@gmail.com>
2024-08-01 21:49:56 +0800

    first commit

---
+++ python.py
@@ -0,0 +1,8 @@
+python
+#Creating and accessing lists 
+fruits=["apple","banna","grapes","orange"]
+print(fruits[0]) # Output:banna
+
+#Modifying lists
+fruits.append ("orange")
+Fruits.remove ("grapes")
